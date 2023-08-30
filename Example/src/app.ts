e5a5ccdbfc4da3d421f7a089f08ccd31d6d2ab09
cch0807 <ckdgus11021@gmail.com>
2023-08-30 20:26:33 +0900

    feat: route 분리, 모듈화 & Cat Update, Delete API

--- Example/src/app.ts
+++ Example/src/app.ts
@@ -1,7 +1,7 @@
 //* Create Read */
 
 import * as express from "express";
-import { Cat, CatType } from "./app.model";
+import catsRouter from "./cats/cats.route";
 
 const app: express.Express = express();
 
@@ -15,63 +15,7 @@ app.use((req, res, next) => {
 //* json middleware
 app.use(express.json());
 
-//* READ 모든 고양이 데이터 조회
-app.get("/cats", (req, res) => {
-  try {
-    const cats = Cat;
-    res.status(200).send({
-      success: true,
-      data: {
-        cats,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* READ 특정 고양이 데이터 조회
-app.get("/cats/:id", (req, res) => {
-  try {
-    const params = req.params;
-    const cat = Cat.find((cat) => {
-      return cat.id === params.id;
-    });
-    console.log(cat);
-    res.status(200).send({
-      success: true,
-      data: {
-        cat,
-      },
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
-
-//* CREATE 새로운 고양이 추가
-app.post("/cats", (req, res) => {
-  try {
-    const data = req.body;
-    console.log(data);
-    Cat.push(data); // create
-    res.status(200).send({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    res.status(400).send({
-      success: false,
-      error: error.message,
-    });
-  }
-});
+app.use(catsRouter);
 
 //* 404 middleware
 app.use((req, res, next) => {
